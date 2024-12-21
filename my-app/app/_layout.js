cc53e60e254d5511bf6e270b7f6f691dda64be48
Jini Byun <jinibyun@gmail.com>
2024-12-21 16:00:37 -0500

    Navigation pattern - Drawer

--- my-app/app/_layout.js
+++ my-app/app/_layout.js
@@ -1,9 +1,35 @@
-import { Stack } from 'expo-router/stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={({ route }) => ({
+            drawerItemStyle: route.name === 'index' || route.name === 'user/[id]' ? {} : { display: 'none' },
+        })}
+      >
+        <Drawer.Screen
+          name="index" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'Home',
+            title: 'overview',
+          }}
+        />
+        <Drawer.Screen
+          name="user/[id]" // This is the name of the page and must match the url from root
+          options={{
+            drawerLabel: 'User',
+            title: 'overview',
+            // href: {
+            //     pathname: '/user/[id]',
+            //     params: {
+            //         id: 'jinibyun',
+            //     },
+            // },
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
---
+++ my-app/app/index.js
@@ -0,0 +1,10 @@
+import { View, Pressable, Text } from 'react-native';
+import { Link} from 'expo-router';
+
+export default function Page() {
+  return (
+    <View>
+      <Text>Text</Text>
+    </View>
+  );
+}
