dbf36af7270ba612717a5b6d12ee5fdeae2972c4
Jini Byun <jinibyun@gmail.com>
2024-12-21 00:18:10 +0000

    basic page navigation with react route

--- my-app/app/index.js
+++ my-app/app/index.js
@@ -1,34 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { View, Pressable, Text } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Page() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+    <View>
+      <Link href="/about">About</Link>
+      {/* ...other links */}
+      <Link href="/user/bacon">View user</Link>
+      <Link href="/other" asChild>
+        <Pressable>
+          <Text>Other</Text>
+        </Pressable>
+      </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
---
+++ my-app/app/other.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function About() {
+  return (
+    <View>
+      <Text>other</Text>
+    </View>
+  );
+}
